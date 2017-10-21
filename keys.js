c61823fd29989065a41947d526e6e63f8a50adec
Fausto Rendon <faustor001@gmail.com>
2017-10-21 16:15:53 -0700

    File setup

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: '28Qrkt9hxFfjleDeXLSj4zCNm',
+  consumer_secret: 'Zy7skfG8pOkq9P166YbMSYwkIiV5j9jG5GKUByGZj7aaKHJ4bt',
+  access_token_key: '921815648878997504-M2RoJykSnUtwaXMjROD6SShEpAu6MYV',
+  access_token_secret: 'sHCRZRA8of0IXA5OCi0kwL3noYT6R4y3WeLuZc8Td0w5n',
+}
+
+module.exports = twitterKeys;
