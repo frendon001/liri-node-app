530ffbcf4b60f8a2689e31e4ad7965c9d1126e37
Fausto Rendon <faustor001@gmail.com>
2017-10-22 17:17:01 -0700

    completed do-what-it-says and bonus

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
 var request = require('request');
 var Spotify = require('node-spotify-api');
+var fs = require("fs");
 var twitterKeys = require('./keys.js')[0];
 var spotifyKeys = require('./keys.js')[1];
 var client = new Twitter(twitterKeys);
@@ -13,42 +14,203 @@ var liriParam = "";
 if (parameters[2]) {
 
 	//get additional paramter for Spotify and OMDB APIs
-	if (liriParam[3]) {
+	if (parameters[3]) {
 		liriParam += parameters[3];
 	}
-	
 
+	//create append additional paramaters entered
 	for (var i = 4; i < parameters.length; i++) {
 		liriParam += " " + parameters[i];
 	}
 
-
 	liriCommand = liriCommand.toLowerCase();
-	console.log(liriCommand);
-	console.log(liriParam);
+
 	switch (liriCommand) {
 		case "my-tweets":
 			//liriCommand matches my-tweets
+			runLiriCommand(liriCommand);
+			// client.get('statuses/user_timeline', { screen_name: 'Gray_OnTime', count: 20 }, function(error, tweets, response) {
+			// 	if (error) throw error;
+
+			// 	//display the last 20 tweets 
+			// 	for (var tweet in tweets) {
+			// 		console.log(tweets[tweet].created_at, tweets[tweet].text);
+			// 		//console.log(tweets[tweet].text);
+
+			// 	}
+
+			// });
+			break;
+		case "spotify-this-song":
+			//liriCommand matches spotify-this-song
+			runLiriCommand(liriCommand, liriParam);
+			// if (!liriParam) {
+			// 	//set default song if none is entered
+			// 	liriParam = "The Sign Ace of Base";
+			// }
+			// spotify
+			// 	.search({ type: 'track', query: liriParam })
+			// 	.then(function(response) {
+			// 		//console.log(response);
+			// 		if (response.tracks.items[0]) {
+			// 			//display track information if available
+			// 			console.log("Artist: ", response.tracks.items[0].artists[0].name);
+			// 			console.log("Song: ", response.tracks.items[0].name);
+			// 			console.log("Preview: ", response.tracks.items[0].preview_url);
+			// 			console.log("Album: ", response.tracks.items[0].album.name);
+			// 		} else {
+			// 			console.log("No results found. Try again.")
+			// 		}
+
+			// 	})
+			// 	.catch(function(err) {
+			// 		console.log(err);
+			// 	});
+			break;
+		case "movie-this":
+			//liriCommand matches movie-this
+			runLiriCommand(liriCommand, liriParam);
+			//build OMDB API URL
+			// requestLink = "http://www.omdbapi.com/?t=" + liriParam + "&y=&plot=short&apikey=40e9cece";
+
+			// if (liriParam) {
+			// 	console.log(requestLink);
+
+			// 	// Then run a request to the OMDB API with the movie specified
+			// 	request(requestLink, function(error, response, body) {
+
+			// 		// If the request is successful (i.e. if the response status code is 200)
+			// 		if (!error && response.statusCode === 200) {
+
+			// 			// Title of the movie.
+			// 			console.log("Title: " + JSON.parse(body).Title);
+			// 			// Year the movie came out.
+			// 			console.log("Year of Release: " + JSON.parse(body).Year);
+			// 			// IMDB Rating of the movie.
+			// 			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			// 			// Rotten Tomatoes Rating of the movie.
+			// 			console.log(JSON.parse(body).Ratings[1].Source + " Rating: " + JSON.parse(body).Ratings[1].Value);
+			// 			// Country where the movie was produced.
+			// 			console.log("Country: " + JSON.parse(body).Country);
+			// 			// Language of the movie.
+			// 			console.log("Language: " + JSON.parse(body).Language);
+			// 			// Plot of the movie.
+			// 			console.log("Plot: " + JSON.parse(body).Plot);
+			// 			// Actors in the movie.
+			// 			console.log("Actors: " + JSON.parse(body).Actors);
+			// 		}
+			// 	});
+
+			// } else {
+			// 	// Then run a request to the OMDB API with the movie specified
+			// 	request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece", function(error, response, body) {
+
+			// 		// If the request is successful (i.e. if the response status code is 200)
+			// 		if (!error && response.statusCode === 200) {
+
+			// 			// Title of the movie.
+			// 			console.log("Title: " + JSON.parse(body).Title);
+			// 			// Year the movie came out.
+			// 			console.log("Year of Release: " + JSON.parse(body).Year);
+			// 			// IMDB Rating of the movie.
+			// 			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			// 			// Rotten Tomatoes Rating of the movie.
+			// 			console.log(JSON.parse(body).Ratings[1].Source + " Rating: " + JSON.parse(body).Ratings[1].Value);
+			// 			// Country where the movie was produced.
+			// 			console.log("Country: " + JSON.parse(body).Country);
+			// 			// Language of the movie.
+			// 			console.log("Language: " + JSON.parse(body).Language);
+			// 			// Plot of the movie.
+			// 			console.log("Plot: " + JSON.parse(body).Plot);
+			// 			// Actors in the movie.
+			// 			console.log("Actors: " + JSON.parse(body).Actors);
+			// 		}
+			// 	});
+			// }
+			break;
+		case "do-what-it-says":
+			//liriCommand matches do-what-it-says
+			writeToLog("****************" + "\n" + liriCommand + "\n");
+			fs.readFile("random.txt", "utf8", function(error, data) {
+				// If the code experiences any errors it will log the error to the console.
+				if (error) {
+					return console.log(error);
+				} else {
+
+					// Then split it by commas (to make it more readable)
+					var dataArr = data.split("\n");
+					var action = [];
+					console.log(dataArr);
+
+					for (var i = 0; i < dataArr.length; i++) {
+
+						action = dataArr[i].split(",");
+						console.log(action);
+						if (action.length === 1) {
+							runLiriCommand(action[0]);
+						} else {
+							runLiriCommand(action[0], action[1]);
+						}
+
+
+					}
+
+				}
+			});
+			break;
+		default:
+			//liriCommand does not match the list of available commands
+			console.log("Please enter a valid liri command from the list below:");
+			console.log(" my-tweets\n", "spotify-this-song\n", "movie-this\n", "do-what-it-says\n");
+			break;
+	}
+
+} else {
+	//No command was provided
+	console.log("Please enter a liri command from the list below:");
+	console.log(" my-tweets\n", "spotify-this-song\n", "movie-this\n", "do-what-it-says\n");
+}
+
+
+function runLiriCommand(command, param) {
+
+
+	switch (command) {
+		case "my-tweets":
+			//add command to log.txt
+			writeToLog("****************" + "\n" + command + "\n" );
+			//liriCommand matches my-tweets
 			client.get('statuses/user_timeline', { screen_name: 'Gray_OnTime', count: 20 }, function(error, tweets, response) {
-				if (error) throw error;
+				if (error) {
+					console.log(error);
+					writeToLog(error);
+				};
+
 
 				//display the last 20 tweets 
 				for (var tweet in tweets) {
+
 					console.log(tweets[tweet].created_at, tweets[tweet].text);
-					//console.log(tweets[tweet].text);
+					writeToLog(tweets[tweet].created_at + " " + tweets[tweet].text + "\n");
 
 				}
 
 			});
 			break;
 		case "spotify-this-song":
+			
 			//liriCommand matches spotify-this-song
-			if (!liriParam) {
+			if (!param) {
 				//set default song if none is entered
-				liriParam = "The Sign Ace of Base";
+				param = "The Sign Ace of Base";
+				//add command to log.txt
+				writeToLog("****************" + "\n" + command + "\n");
+			}else{
+				//add command to log.txt
+				writeToLog("****************" + "\n" + command + " " + param + "\n");
 			}
 			spotify
-				.search({ type: 'track', query: liriParam })
+				.search({ type: 'track', query: param })
 				.then(function(response) {
 					//console.log(response);
 					if (response.tracks.items[0]) {
@@ -57,23 +219,32 @@ if (parameters[2]) {
 						console.log("Song: ", response.tracks.items[0].name);
 						console.log("Preview: ", response.tracks.items[0].preview_url);
 						console.log("Album: ", response.tracks.items[0].album.name);
+
+						writeToLog("Artist: " + response.tracks.items[0].artists[0].name + "\n" +
+							"Song: " + response.tracks.items[0].name + "\n" +
+							"Preview: " + response.tracks.items[0].preview_url + "\n" +
+							"Album: " + response.tracks.items[0].album.name + "\n"
+						);
 					} else {
 						console.log("No results found. Try again.")
+						writeToLog("No results found. Try again.");
 					}
 
 				})
 				.catch(function(err) {
 					console.log(err);
+					riteToLog(err);
 				});
 			break;
 		case "movie-this":
 			//liriCommand matches movie-this
 
 			//build OMDB API URL
-			requestLink = "http://www.omdbapi.com/?t=" + liriParam + "&y=&plot=short&apikey=40e9cece";
+			requestLink = "http://www.omdbapi.com/?t=" + param + "&y=&plot=short&apikey=40e9cece";
 
-			if (liriParam) {
-				console.log(requestLink);
+			if (param) {
+				//add command to log.txt
+				writeToLog("****************" + "\n" + command + " " + param + "\n");
 
 				// Then run a request to the OMDB API with the movie specified
 				request(requestLink, function(error, response, body) {
@@ -97,10 +268,22 @@ if (parameters[2]) {
 						console.log("Plot: " + JSON.parse(body).Plot);
 						// Actors in the movie.
 						console.log("Actors: " + JSON.parse(body).Actors);
+
+						writeToLog("Title: " + JSON.parse(body).Title + "\n"
+							+ "Year of Release: " + JSON.parse(body).Year + "\n"
+							+ "IMDB Rating: " + JSON.parse(body).imdbRating + "\n"
+							+ JSON.parse(body).Ratings[1].Source + " Rating: " + JSON.parse(body).Ratings[1].Value + "\n"
+							+ "Language: " + JSON.parse(body).Language+ "\n"
+							+ "Plot: " + JSON.parse(body).Plot + "\n"
+							+ "Actors: " + JSON.parse(body).Actors + "\n"
+						);
+
 					}
 				});
 
 			} else {
+				//add command to log.txt
+				writeToLog("****************" + "\n" + command + "\n");
 				// Then run a request to the OMDB API with the movie specified
 				request("http://www.omdbapi.com/?t=mr+nobody&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
@@ -123,12 +306,17 @@ if (parameters[2]) {
 						console.log("Plot: " + JSON.parse(body).Plot);
 						// Actors in the movie.
 						console.log("Actors: " + JSON.parse(body).Actors);
+						writeToLog("Title: " + JSON.parse(body).Title + "\n" +
+							"Year of Release: " + JSON.parse(body).Year + "\n" +
+							"IMDB Rating: " + JSON.parse(body).imdbRating + "\n" +
+							JSON.parse(body).Ratings[1].Source + " Rating: " + JSON.parse(body).Ratings[1].Value + "\n" +
+							"Language: " + JSON.parse(body).Language + "\n" +
+							"Plot: " + JSON.parse(body).Plot + "\n" +
+							"Actors: " + JSON.parse(body).Actors + "\n"
+						);
 					}
 				});
 			}
-			break;
-		case "do-what-it-says":
-			//liriCommand matches do-what-it-says
 			break;
 		default:
 			//liriCommand does not match the list of available commands
@@ -136,12 +324,19 @@ if (parameters[2]) {
 			console.log(" my-tweets\n", "spotify-this-song\n", "movie-this\n", "do-what-it-says\n");
 			break;
 	}
+};
 
-} else {
-	console.log("Please enter a valid liri from the list below:");
-	console.log(" my-tweets\n", "spotify-this-song\n", "movie-this\n", "do-what-it-says\n");
-}
+function writeToLog(content) {
+	fs.appendFile("log.txt", content, function(err) {
+		// If an error was experienced we say it.
+		if (err) {
+			console.log(err);
+		} else {
+			//content was added to log.txt
+		}
+	});
 
+};
 // // We then run the request module on a URL with a JSON
 // request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=40e9cece", function(error, response, body) {
 
